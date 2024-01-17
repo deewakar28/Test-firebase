3ad9e05582b6f9b585dbcd89744e37df616b7a04
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-17 19:56:14 +0530

    auth added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { Routes,Route } from 'react-router-dom'
 import HomePage from './pages/home'
 import RoomPage from './pages/room'
 import './App.css'
+import Mentee from './pages/Signin/Mentee'
+import Mentor from './pages/Signin/Mentor'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
+      <Route path='/' element={<HomePage/>}/>
+        <Route path='/Mentor' element={<Mentor/>}/>
+        <Route path='/Mentee' element={<Mentee/>}/>
         <Route path='/room/:roomId' element={<RoomPage/>} />
       </Routes>
     </>
